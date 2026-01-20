f03affe1a14b466f3290004a3ee780f74b685db3
vincent <857401025@qq.com>
2026-01-20 15:52:42 +0800

    nextjs框架

--- backend/server/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import JsonResponse
-
-def api_root(request):
-    return JsonResponse({
-        "message": "Welcome to the SubscribeMeForAI API",
-        "status": "running"
-    })
